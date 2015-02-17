7865f4bb5a21fc8fc8359509bda4e79ca6a560da
Angus Peart <angusp@gmail.com>
2015-02-16 19:34:03 -0800

    Cleaned up header file generation
    Ported Perl make_header script to Python to fall in line with the
    existing 3DR toolset.

--- AZBootloader/make_header.bat
+++ AZBootloader/make_header.bat
@@ -1,6 +1,6 @@
 cd ..
 del data.h
-C:\ti\ccsv6\tools\compiler\c2000_6.2.10\bin\hex2000.exe "C:\Users\angus_000\Google Drive\Gimbal\solo-gimbal-aes\GimbalFirmware\F2806x_RAM\PM_Sensorless_F2806x.out" -o data.hh -boot -gpio8 -a 
-C:\Perl64\bin\perl.exe make_header.pl
-del data.hh
+C:\ti\ccsv6\tools\compiler\c2000_6.2.10\bin\hex2000.exe "GimbalFirmware\F2806x_RAM\PM_Sensorless_F2806x.out" -o data.bin -boot -gpio8 -b 
+python tools\make_header.py data.bin data.h
+del data.bin
 pause
--- GimbalFirmware/make_header.bat
+++ GimbalFirmware/make_header.bat
@@ -1,1 +1,1 @@
-C:\ti\ccsv6\tools\compiler\c2000_6.2.10\bin\hex2000.exe "C:\Users\angus_000\Google Drive\Gimbal\solo-gimbal-aes\GimbalFirmware\F2806x_RAM\PM_Sensorless_F2806x.out" -o Gimbal.bin -boot -gpio8 -b
+C:\ti\ccsv6\tools\compiler\c2000_6.2.10\bin\hex2000.exe "F2806x_RAM\PM_Sensorless_F2806x.out" -o Gimbal.bin -boot -gpio8 -b
