bb8d634b2208c571759951dfff98f48ea93850d2
Angus Peart <angusp@gmail.com>
2015-02-16 20:17:40 -0800

    Update python fw loader
    - Checksum is calculated on the fly from binary
    - Input file is input as an argument

--- Tools/loadfw.py
+++ Tools/loadfw.py
@@ -6,16 +6,16 @@ Utility for loading firmware into the 3DR Gimbal.
 @author: Angus Peart (angus@3dr.com)
 """
 
-import sys, signal
+import sys, signal, argparse
 from pymavlink import mavutil
 from pymavlink.dialects.v10 import common as mavlink
-from data import *
 
 MAVLINK_SYSTEM_ID = 50
 MAVLINK_COMPONENT_ID = 230
 
 MAVLINK_ENCAPSULATED_DATA_LENGTH = 253
 
+# TODO: Add these as arguments instead of being hardcoded
 serialport = "COM7"
 baudrate = 230400
 
@@ -45,11 +45,52 @@ def wait_handshake(m):
 #             print(msg)
 
 def load_binary(filename):
-    '''load binary image file into a byte array'''
+    '''Load binary image file into a byte array'''
     with open(filename, "rb") as f:
         return bytearray(f.read())
 
+def bytearray_to_wordarray(data):
+    '''Converts an 8-bit byte array into a 16-bit word array'''
+    wordarray = list()
+
+    for i in range(len(data)/2):
+        # Calculate 16 bit word from two bytes
+        msb = data[(i*2)+0]
+        lsb = data[(i*2)+1]
+        word = (msb << 8) | lsb
+        wordarray.append(word)
+
+    return wordarray
+
+def append_checksum(binary):
+    '''Calculate and append the XOR checksum to the bytearray'''
+    checksum = 0xFFFF
+    wordarray = bytearray_to_wordarray(binary)
+
+    # Compute the checksum
+    for i in range(len(wordarray)):
+        checksum ^= wordarray[i]
+    print("Checksum: 0x%04X" % checksum)
+
+    # Add the checksum to the end of the wordarray
+    wordarray.extend([checksum&0xFFFF, (checksum&0xFFFF)>>16, 0x0000])
+
+    # Convert the wordarray back into a bytearray
+    barray = list()
+    for i in range(len(wordarray)):
+        lsb = wordarray[i] & 0xFF
+        msb = (wordarray[i] >> 8) & 0xFF
+        barray.append(lsb)
+        barray.append(msb)
+
+    return barray
+
+
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("binary", help="application binary file load")
+    args = parser.parse_args()
+
     # Open the serial port
     mavserial = mavutil.mavserial(
         device = serialport,
@@ -58,8 +99,9 @@ def main():
     link = mavlink.MAVLink(mavserial, MAVLINK_SYSTEM_ID, MAVLINK_COMPONENT_ID)
 
     # Load the binary image into a byte array
-    #binary = load_binary("Gimbal.bin")
-    binary = bytearray(firmware_array)
+    print("Application binary: %s" % args.binary)
+    hexfile = load_binary(args.binary)
+    binary = append_checksum(hexfile)
 
     # Wait for a handshake from the gimbal which contains the payload length
     sys.stdout.write("Uploading firmware to gimbal ")
