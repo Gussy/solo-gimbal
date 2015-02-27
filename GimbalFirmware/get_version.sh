9dafa5ac8d8d2ea8c836d476c4bc15f633a07e98
Angus Peart <angusp@gmail.com>
2015-02-27 14:04:29 -0800

    Removed reliance on sh being on PATH
    ... and replaced it with a reliance on git being on PATH. Still, it's
    more likely for Windows to have git than sh on the path.

--- GimbalFirmware/get_version.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git describe --tags --dirty --long | /bin/sed -e 's/.*/#define GitVersionString "&"/' > version.tmp
-git branch | grep '*' | /bin/sed -e 's/\*\ \(.*\)/#define GitBranch "\1"/' >> version.tmp
-git describe --tags --long | /bin/sed -e 's/\(v[0-9]*\.[0-9]*\.[0-9]*\).*/#define GitTag "\1"/' >> version.tmp
-git describe --tags --long | /bin/sed -e 's/v[0-9]*\.[0-9]*\.[0-9]*-\([0-9]*\)-.*/#define GitCommit "\1"/' >> version.tmp
-git describe --tags --long | /bin/sed -e 's/v\([0-9]*\)\.\([0-9]*\)\.\([0-9]*\).*/#define GitVersionMajor "\1"\n#define GitVersionMinor "\2"\n#define GitVersionRevision "\3"/' >> version.tmp
-git describe --tags --long | /bin/sed -e 's/v\([0-9]*\)\.\([0-9]*\)\.\([0-9]*\).*/#define GitVersionMajorInt \1\n#define GitVersionMinorInt \2\n#define GitVersionRevisionInt \3/' >> version.tmp
-cmp -s Headers/version.h version.tmp && rm -f version.tmp || mv -f version.tmp Headers/version.h
