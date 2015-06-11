28de565369d2c620cd68fea4efe59b1c67f64cfa
Arthur Benemann <arthur.benemann@gmail.com>
2015-06-10 22:59:24 -0300

    tools: add validation for joint,gyro and accelerometers offsets

--- Tools/src/setup_check.py
+++ Tools/src/setup_check.py
@@ -44,6 +44,39 @@ def validate_comutation(link):
         print 'Comutation \t- FAIL - please redo the comutation calibration (-c)'
 
 
+def validate_joints(link):
+    "The acceptable range values where collected from the batch of DVT1 gimbals"
+    joint = setup_param.get_offsets(link, 'JNT')
+    if ((joint.x <= 0.12) and (joint.x >= -0.02) and (joint.x != 0) and
+        (joint.y <= 0.15) and (joint.y >= -0.07) and (joint.y != 0) and
+        (joint.z <= 0.18) and (joint.z >= -0.18) and (joint.z != 0)):
+        print 'Joints  \t- PASS'
+    else:
+        print 'Joints  \t- FAIL - redo joint calibration (-j)'
+
+def validate_gyros(link):
+    "The acceptable range values where collected from the batch of DVT1 gimbals"
+    gyro = setup_param.get_offsets(link, 'GYRO')
+    if ((gyro.x <= 5E-04) and (gyro.x >= -5E-04) and (gyro.x != 0) and
+        (gyro.y <= 5E-04) and (gyro.y >= -5E-04) and (gyro.y != 0) and
+        (gyro.z <= 5E-04) and (gyro.z >= -5E-04) and (gyro.z != 0)):
+        print 'Gyros   \t- PASS'
+    else:
+        print 'Gyros   \t- FAIL - redo gyro calibration (-g)'
+
+def validate_accelerometers(link):
+    "Since there is no accelerometer cal yet, just check if the values are zeroed"
+    acc = setup_param.get_offsets(link, 'ACC')
+    if ((acc.x == 0) and
+        (acc.y == 0) and
+        (acc.z == 0)):
+        print 'Accelerometer\t- PASS'
+    else:
+        print 'Accelerometer\t- FAIL - redo accelerometer calibration (-a)'
+
 def validate(link):
     validate_version(link)
     validate_comutation(link)
+    validate_joints(link)
+    validate_gyros(link)
+    validate_accelerometers(link)
