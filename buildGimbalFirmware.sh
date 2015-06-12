fde9e933d1d46b12c7ac7585b2f2ad68ca5e6c5b
Arthur Benemann <arthur.benemann@gmail.com>
2015-06-12 17:13:01 -0300

    build: clean-up of files after build

--- buildGimbalFirmware.sh
+++ buildGimbalFirmware.sh
@@ -6,3 +6,10 @@ cd GimbalFirmware/build
 pwd
 ls -l /tmp/ti/ccsv6/utils/bin/
 /tmp/ti/ccsv6/utils/bin/gmake -k all
+
+# clean-up
+find . -name '*.pp' -delete
+find . -name '*.obj' -delete
+find . -name '*.mk' -delete
+find . -name '*.pyc' -delete
+find . -name '*org.eclipse*' -delete
