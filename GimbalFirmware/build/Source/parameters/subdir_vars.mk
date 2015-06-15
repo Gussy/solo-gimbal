44d48d2ee981f7736c16ab705857a5b06b033676
Arthur Benemann <arthur.benemann@gmail.com>
2015-06-14 21:28:24 -0300

    travis: update makefile

--- GimbalFirmware/build/Source/parameters/subdir_vars.mk
+++ GimbalFirmware/build/Source/parameters/subdir_vars.mk
@@ -3,22 +3,15 @@
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
-ASM_SRCS += \
-../Source/parameters/Example_Flash2806x_CsmKeys.asm 
-
 C_SRCS += \
 ../Source/parameters/flash_params.c \
 ../Source/parameters/load_axis_parms_state_machine.c \
 ../Source/parameters/mavlink_parameter_interface.c 
 
 OBJS += \
-./Example_Flash2806x_CsmKeys.obj \
 ./flash_params.obj \
 ./load_axis_parms_state_machine.obj \
 ./mavlink_parameter_interface.obj 
-
-ASM_DEPS += \
-./Source/parameters/Example_Flash2806x_CsmKeys.pp 
 
 C_DEPS += \
 ./Source/parameters/flash_params.pp \
@@ -31,16 +24,9 @@ C_DEPS__QUOTED += \
 "Source/parameters/mavlink_parameter_interface.pp" 
 
 OBJS__QUOTED += \
-"Example_Flash2806x_CsmKeys.obj" \
 "flash_params.obj" \
 "load_axis_parms_state_machine.obj" \
 "mavlink_parameter_interface.obj" 
-
-ASM_DEPS__QUOTED += \
-"Source/parameters/Example_Flash2806x_CsmKeys.pp" 
-
-ASM_SRCS__QUOTED += \
-"../Source/parameters/Example_Flash2806x_CsmKeys.asm" 
 
 C_SRCS__QUOTED += \
 "../Source/parameters/flash_params.c" \
