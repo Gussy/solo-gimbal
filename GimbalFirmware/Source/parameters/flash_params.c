047d3dfda2072b3d7c8d4143330aabe7e7c72bb0
Arthur Benemann <arthur.benemann@gmail.com>
2015-06-19 16:04:28 -0300

    flash_params: shuffle position of parameters in flash

--- GimbalFirmware/Source/parameters/flash_params.c
+++ GimbalFirmware/Source/parameters/flash_params.c
@@ -1,90 +1,39 @@
 #include "parameters/flash_params.h"
-#include "version_git.h"
+
+// ***************************************************************
+// NOTE: These differ per gimbal, and are loaded from flash at boot
+// time, so we default these to 0 here to force and auto-calibration
+// if there are no previously saved
 
 struct flash_param_struct_0000 flash_params =
 {
-    0x0000,                     // Flash Struct ID
-	0x00000000,                 // Software version number, loaded from compiled in version information at boot time
-    0x00000000,                 // Assembly time
-    0x00000000,                 // Serial number part 1 (part code, design, language/country)
-    0x00000000,                 // Serial number part 2 (option, year, month)
-    0x00000000,                 // Serial number part 3 (incrementing serial number per month)
-    // ***************************************************************
-    // NOTE: These differ per gimbal, and are loaded from flash at boot
-    // time, so we default these to 0 here to force and auto-calibration
-    // if there are no previously saved
-    // Axis calibration slopes
-    {
-        0.0,       // EL
-        0.0,       // AZ
-        0.0        // ROLL
-    },
-    // Axis calibration intercepts
-    {
-        0.0,      // EL
-        0.0,      // AZ
-        0.0       // ROLL
-    },
-    // Rate PID P gains
-    {
-        3.5,    // EL
-        4.0,    // AZ
-        4.0     // ROLL
-    },
-    // Rate PID I gains
-    {
-        0.25,   // EL
-        1.0,    // AZ
-        0.75    // ROLL
-    },
-    // Rate PID D gains
-    {
-        0.1,    // EL
-        0.1,    // AZ
-        1.0     // ROLL
-    },
-    // Rate PID windup limits
-    {
-        32768.0,// EL
-        32768.0,// AZ
-        32768.0 // ROLL
-    },
-    // Torque Loop PID Kp
-    {
-        0.8,    // EL
-        0.8,    // AZ
-        0.8     // ROLL
-    },
-    // Torque Loop PID Ki
-    {
-        0.75,   // EL
-        0.75,   // AZ
-        0.75    // ROLL
-    },
-    // Torque Loop PID Kd
-    {
-        0.0,    // EL
-        0.0,    // AZ
-        0.0     // ROLL
-    },
-    // offset_joint
-    {
-        0.0,    // X
-        0.0,    // Y
-        0.0     // Z
-    },
-    // offset_accelerometers
-    {
-        0.0,    // X
-        0.0,    // Y
-        0.0     // Z
-    },
-    // offset_gyro
-    {
-        0.0,    // X
-        0.0,    // Y
-        0.0     // Z
-    },
-    0.0,			// Pointing loop gain
-	0.0,				// Message brodcasting
+		0x0001,                     // Flash Struct ID
+		0x00000000,                 // Software version number, loaded from compiled in version information at boot time
+		0x00000000,                 // Assembly time
+		0x00000000,                 // Serial number part 1 (part code, design, language/country)
+		0x00000000,                 // Serial number part 2 (option, year, month)
+		0x00000000,                 // Serial number part 3 (incrementing serial number per month)
+
+		0.0,							// Message brodcasting
+		0.0,						// Pointing loop gain
+
+	//	{EL, AZ, ROLL}
+		{ 0.0, 0.0, 0.0 },   		// Axis calibration slopes
+		{ 0.0, 0.0, 0.0 },			// Axis calibration intercepts
+
+	//	{EL, AZ, ROLL}
+    	{ 0.80, 0.80, 0.80 },		// Torque Loop PID Kp
+    	{ 0.75, 0.75, 0.75 },		// Torque Loop PID Ki
+    	{ 0.00, 0.00, 0.00 },		// Torque Loop PID Kd
+
+	//	{EL, AZ, ROLL}
+		{ 0.0, 0.0, 0.0 },			// Rate PID P gains
+		{ 0.0, 0.0, 0.0 },			// Rate PID I gains
+    	{ 0.0, 0.0, 0.0 },			// Rate PID D gains
+    	{ 32768.0,32768.0,32768.0 },// Rate PID windup limits
+
+	// 	{ X,   Y,   Z   }
+		{ 0.0, 0.0, 0.0 },			// offset_joint
+		{ 0.0, 0.0, 0.0 },			// offset_accelerometers
+		{ 0.0, 0.0, 0.0 },			// offset_gyro
 };
--- shared/headers/parameters/flash_params.h
+++ shared/headers/parameters/flash_params.h
@@ -11,20 +11,20 @@ struct flash_param_struct_0000 {
 	Uint32 ser_num_1;
 	Uint32 ser_num_2;
 	Uint32 ser_num_3;
+	float broadcast_msgs;
+	float k_rate;
 	float commutation_slope[AXIS_CNT];
 	float commutation_icept[AXIS_CNT];
+	float torque_pid_kp[AXIS_CNT];
+	float torque_pid_ki[AXIS_CNT];
+	float torque_pid_kd[AXIS_CNT];
 	float rate_pid_p[AXIS_CNT];
 	float rate_pid_i[AXIS_CNT];
 	float rate_pid_d[AXIS_CNT];
 	float rate_pid_windup[AXIS_CNT];
-	float torque_pid_kp[AXIS_CNT];
-	float torque_pid_ki[AXIS_CNT];
-	float torque_pid_kd[AXIS_CNT];
 	float offset_joint[AXIS_CNT];
 	float offset_accelerometer[AXIS_CNT];
 	float offset_gyro[AXIS_CNT];
-	float k_rate;
-	float broadcast_msgs;
 };
 
 extern struct flash_param_struct_0000 flash_params;
