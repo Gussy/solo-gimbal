1ae827c05bc9a8ce596080d6cd3c637093ab3aa4
Arthur Benemann <arthur.benemann@gmail.com>
2015-06-20 05:32:50 -0300

    tools: simple UI for accelcal

--- Tools/src/setup_home.py
+++ Tools/src/setup_home.py
@@ -10,18 +10,18 @@ from pymavlink.rotmat import Vector3
 NUMBER_OF_SAMPLES = 200
 ENCODER_COUNTS_PER_RADIAN = 1000.0/(2*math.pi)
 
-def getAverage(link,get_variable):
+def getAverage(link,get_variable, sample_count =  NUMBER_OF_SAMPLES):
     sum_angles = Vector3()
-    for i in range(NUMBER_OF_SAMPLES):
+    for i in range(sample_count):
         angles = get_variable(link)
         sum_angles += angles    
-    offset = sum_angles/NUMBER_OF_SAMPLES
-    print [offset.x,offset.y,offset.z]
+    offset = sum_angles/sample_count
     return offset
 
 def calibrate_joints(link):
     message_brodcasting(link, True)
     average = getAverage(link, get_current_joint_angles)
+    print [average.x,average.y,average.z]
     set_offsets(link,'JNT',average)
     message_brodcasting(link, False)
     return
@@ -29,13 +29,34 @@ def calibrate_joints(link):
 def calibrate_gyro(link):
     message_brodcasting(link, True)
     average = getAverage(link, get_current_delta_angles)
+    print [average.x,average.y,average.z]
     set_offsets(link,'GYRO',average)
     message_brodcasting(link, False)
     return
 
 def calibrate_accel(link):
     message_brodcasting(link, True)
-    average = getAverage(link, get_current_delta_velocity)
+    
+    # Get samples
+    samples = []; AVG_COUNT =50
+    raw_input("Place the gimbal leveled")
+    samples.append(getAverage(link, get_current_delta_velocity, sample_count=AVG_COUNT))
+    raw_input("Place the gimbal on it's side")
+    samples.append(getAverage(link, get_current_delta_velocity, sample_count=AVG_COUNT))
+    raw_input("Place the gimbal on it's back")
+    samples.append(getAverage(link, get_current_delta_velocity, sample_count=AVG_COUNT))
+    raw_input("Place the gimbal on it's other side")
+    samples.append(getAverage(link, get_current_delta_velocity, sample_count=AVG_COUNT))
+    raw_input("Place the gimbal on it's front side")
+    samples.append(getAverage(link, get_current_delta_velocity, sample_count=AVG_COUNT))
+    raw_input("Place the gimbal upside-down")
+    samples.append(getAverage(link, get_current_delta_velocity, sample_count=AVG_COUNT))
+    
+    # Apply Math
+    for vector in samples:
+        print vector
+        
+    # Save values
     set_offsets(link, 'ACC', Vector3()) # TODO: implement accel calibration
     message_brodcasting(link, False)
     return
