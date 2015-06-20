78d60268527a6a55506ed1bd64347dbe61b2dfe7
Arthur Benemann <arthur.benemann@gmail.com>
2015-06-20 04:15:25 -0300

    tools: display the serial number on the -show option

--- Tools/src/setup_validate.py
+++ Tools/src/setup_validate.py
@@ -46,14 +46,15 @@ EXPECTED_K_RATE = 10.0
 
 def show(link):
     ver = setup_factory.readSWver(link)
+    asm_date = setup_factory.get_assembly_time(link)
+    serial = setup_factory.get_serial_number(link)
     pitch_com, roll_com, yaw_com = setup_comutation.getAxisCalibrationParams(link)
     joint = setup_param.get_offsets(link, 'JNT')
     gyro = setup_param.get_offsets(link, 'GYRO')
     acc = setup_param.get_offsets(link, 'ACC')
     k_rate = setup_param.fetch_param(link, "GMB_K_RATE").param_value
-    asm_date = setup_factory.get_assembly_time(link)
-    print "sw_ver, asm_date, pitch_icept, pitch_slope, roll_icept, roll_slope, yaw_icept, yaw_slope, joint_z, joint_y, joint_z, gyro_x, gyro_y, gyro_z, acc_x, acc_y, acc_z, k_rate"
-    print "%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s" % (ver, asm_date, pitch_com[1], pitch_com[2], roll_com[1], roll_com[2], yaw_com[1], yaw_com[2], joint.x, joint.y, joint.z, gyro.x, gyro.y, gyro.z, acc.x, acc.y, acc.z, k_rate)
+    print "sw_ver, serial, asm_date, pitch_icept, pitch_slope, roll_icept, roll_slope, yaw_icept, yaw_slope, joint_z, joint_y, joint_z, gyro_x, gyro_y, gyro_z, acc_x, acc_y, acc_z, k_rate"
+    print "%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s" % (ver, serial, asm_date, pitch_com[1], pitch_com[2], roll_com[1], roll_com[2], yaw_com[1], yaw_com[2], joint.x, joint.y, joint.z, gyro.x, gyro.y, gyro.z, acc.x, acc.y, acc.z, k_rate)
     
 
 def validate_version(link):
