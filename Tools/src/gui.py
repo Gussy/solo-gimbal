f30a49b5483771bab30663d8c043117e8f050c34
Angus Peart <angusp@gmail.com>
2015-06-24 14:26:35 +0800

    tools: gui support for gimbal validation

--- Tools/src/gui.py
+++ Tools/src/gui.py
@@ -2,7 +2,7 @@ import sys, time
 from PySide import QtCore, QtGui
 from PySide.QtCore import QThread, Slot
 from qtasync import AsyncTask, coroutine
-import setup_mavlink, setup_read_sw_version
+import setup_mavlink, setup_read_sw_version, setup_validate
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -16,6 +16,7 @@ class Ui_MainWindow(object):
         self.centralWidget = QtGui.QWidget(MainWindow)
         self.centralWidget.setObjectName("centralWidget")
         self.tabWidget = QtGui.QTabWidget(self.centralWidget)
+        self.tabWidget.setEnabled(False)
         self.tabWidget.setGeometry(QtCore.QRect(10, 149, 581, 341))
         self.tabWidget.setTabPosition(QtGui.QTabWidget.North)
         self.tabWidget.setTabShape(QtGui.QTabWidget.Rounded)
@@ -24,10 +25,97 @@ class Ui_MainWindow(object):
         self.tabWidget.setTabsClosable(False)
         self.tabWidget.setMovable(False)
         self.tabWidget.setObjectName("tabWidget")
-        self.tabWidget.setEnabled(False)
         self.tabTests = QtGui.QWidget()
         self.tabTests.setEnabled(True)
         self.tabTests.setObjectName("tabTests")
+        self.formLayoutWidget_4 = QtGui.QWidget(self.tabTests)
+        self.formLayoutWidget_4.setGeometry(QtCore.QRect(10, 70, 551, 231))
+        self.formLayoutWidget_4.setObjectName("formLayoutWidget_4")
+        self.frmlytValidationResults = QtGui.QFormLayout(self.formLayoutWidget_4)
+        self.frmlytValidationResults.setContentsMargins(0, 0, 0, 0)
+        self.frmlytValidationResults.setObjectName("frmlytValidationResults")
+        self.lblVersionLabel = QtGui.QLabel(self.formLayoutWidget_4)
+        self.lblVersionLabel.setEnabled(True)
+        font = QtGui.QFont()
+        font.setPointSize(8)
+        font.setWeight(75)
+        font.setBold(True)
+        self.lblVersionLabel.setFont(font)
+        self.lblVersionLabel.setObjectName("lblVersionLabel")
+        self.frmlytValidationResults.setWidget(0, QtGui.QFormLayout.LabelRole, self.lblVersionLabel)
+        self.lblValidationCommutationLabel = QtGui.QLabel(self.formLayoutWidget_4)
+        font = QtGui.QFont()
+        font.setPointSize(8)
+        font.setWeight(75)
+        font.setBold(True)
+        self.lblValidationCommutationLabel.setFont(font)
+        self.lblValidationCommutationLabel.setObjectName("lblValidationCommutationLabel")
+        self.frmlytValidationResults.setWidget(1, QtGui.QFormLayout.LabelRole, self.lblValidationCommutationLabel)
+        self.lblValidationJointsLabel = QtGui.QLabel(self.formLayoutWidget_4)
+        font = QtGui.QFont()
+        font.setPointSize(8)
+        font.setWeight(75)
+        font.setBold(True)
+        self.lblValidationJointsLabel.setFont(font)
+        self.lblValidationJointsLabel.setObjectName("lblValidationJointsLabel")
+        self.frmlytValidationResults.setWidget(2, QtGui.QFormLayout.LabelRole, self.lblValidationJointsLabel)
+        self.lblValidationGyrosLabel = QtGui.QLabel(self.formLayoutWidget_4)
+        font = QtGui.QFont()
+        font.setPointSize(8)
+        font.setWeight(75)
+        font.setBold(True)
+        self.lblValidationGyrosLabel.setFont(font)
+        self.lblValidationGyrosLabel.setObjectName("lblValidationGyrosLabel")
+        self.frmlytValidationResults.setWidget(3, QtGui.QFormLayout.LabelRole, self.lblValidationGyrosLabel)
+        self.lblValidationAccelerometerLabel = QtGui.QLabel(self.formLayoutWidget_4)
+        font = QtGui.QFont()
+        font.setPointSize(8)
+        font.setWeight(75)
+        font.setBold(True)
+        self.lblValidationAccelerometerLabel.setFont(font)
+        self.lblValidationAccelerometerLabel.setObjectName("lblValidationAccelerometerLabel")
+        self.frmlytValidationResults.setWidget(4, QtGui.QFormLayout.LabelRole, self.lblValidationAccelerometerLabel)
+        self.lblValidationGainsLabel = QtGui.QLabel(self.formLayoutWidget_4)
+        font = QtGui.QFont()
+        font.setPointSize(8)
+        font.setWeight(75)
+        font.setBold(True)
+        self.lblValidationGainsLabel.setFont(font)
+        self.lblValidationGainsLabel.setObjectName("lblValidationGainsLabel")
+        self.frmlytValidationResults.setWidget(5, QtGui.QFormLayout.LabelRole, self.lblValidationGainsLabel)
+        self.lblValidationVersion = QtGui.QLabel(self.formLayoutWidget_4)
+        self.lblValidationVersion.setText("")
+        self.lblValidationVersion.setObjectName("lblValidationVersion")
+        self.frmlytValidationResults.setWidget(0, QtGui.QFormLayout.FieldRole, self.lblValidationVersion)
+        self.lblValidationCommutation = QtGui.QLabel(self.formLayoutWidget_4)
+        self.lblValidationCommutation.setEnabled(True)
+        self.lblValidationCommutation.setText("")
+        self.lblValidationCommutation.setObjectName("lblValidationCommutation")
+        self.frmlytValidationResults.setWidget(1, QtGui.QFormLayout.FieldRole, self.lblValidationCommutation)
+        self.lblValidationJoints = QtGui.QLabel(self.formLayoutWidget_4)
+        self.lblValidationJoints.setText("")
+        self.lblValidationJoints.setObjectName("lblValidationJoints")
+        self.frmlytValidationResults.setWidget(2, QtGui.QFormLayout.FieldRole, self.lblValidationJoints)
+        self.lblValidationGyros = QtGui.QLabel(self.formLayoutWidget_4)
+        self.lblValidationGyros.setText("")
+        self.lblValidationGyros.setObjectName("lblValidationGyros")
+        self.frmlytValidationResults.setWidget(3, QtGui.QFormLayout.FieldRole, self.lblValidationGyros)
+        self.lblValidationAccelerometer = QtGui.QLabel(self.formLayoutWidget_4)
+        self.lblValidationAccelerometer.setText("")
+        self.lblValidationAccelerometer.setObjectName("lblValidationAccelerometer")
+        self.frmlytValidationResults.setWidget(4, QtGui.QFormLayout.FieldRole, self.lblValidationAccelerometer)
+        self.lblValidationGains = QtGui.QLabel(self.formLayoutWidget_4)
+        self.lblValidationGains.setText("")
+        self.lblValidationGains.setObjectName("lblValidationGains")
+        self.frmlytValidationResults.setWidget(5, QtGui.QFormLayout.FieldRole, self.lblValidationGains)
+        self.btnValidationRunTests = QtGui.QPushButton(self.tabTests)
+        self.btnValidationRunTests.setEnabled(True)
+        self.btnValidationRunTests.setGeometry(QtCore.QRect(10, 10, 271, 51))
+        self.btnValidationRunTests.setObjectName("btnValidationRunTests")
+        self.btnValidationSetDefaults = QtGui.QPushButton(self.tabTests)
+        self.btnValidationSetDefaults.setEnabled(True)
+        self.btnValidationSetDefaults.setGeometry(QtCore.QRect(290, 10, 271, 51))
+        self.btnValidationSetDefaults.setObjectName("btnValidationSetDefaults")
         self.tabWidget.addTab(self.tabTests, "")
         self.tabFirmware = QtGui.QWidget()
         self.tabFirmware.setObjectName("tabFirmware")
@@ -135,6 +223,14 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "3DR Gimbal Tools", None, QtGui.QApplication.UnicodeUTF8))
+        self.lblVersionLabel.setText(QtGui.QApplication.translate("MainWindow", "Version:", None, QtGui.QApplication.UnicodeUTF8))
+        self.lblValidationCommutationLabel.setText(QtGui.QApplication.translate("MainWindow", "Commutation:", None, QtGui.QApplication.UnicodeUTF8))
+        self.lblValidationJointsLabel.setText(QtGui.QApplication.translate("MainWindow", "Joints:", None, QtGui.QApplication.UnicodeUTF8))
+        self.lblValidationGyrosLabel.setText(QtGui.QApplication.translate("MainWindow", "Gyros:", None, QtGui.QApplication.UnicodeUTF8))
+        self.lblValidationAccelerometerLabel.setText(QtGui.QApplication.translate("MainWindow", "Accelerometer:", None, QtGui.QApplication.UnicodeUTF8))
+        self.lblValidationGainsLabel.setText(QtGui.QApplication.translate("MainWindow", "Gains:", None, QtGui.QApplication.UnicodeUTF8))
+        self.btnValidationRunTests.setText(QtGui.QApplication.translate("MainWindow", "Run Tests", None, QtGui.QApplication.UnicodeUTF8))
+        self.btnValidationSetDefaults.setText(QtGui.QApplication.translate("MainWindow", "Set Defaults", None, QtGui.QApplication.UnicodeUTF8))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tabTests), QtGui.QApplication.translate("MainWindow", "Tests", None, QtGui.QApplication.UnicodeUTF8))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tabFirmware), QtGui.QApplication.translate("MainWindow", "Firmware", None, QtGui.QApplication.UnicodeUTF8))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tabCalibration), QtGui.QApplication.translate("MainWindow", "Calibration", None, QtGui.QApplication.UnicodeUTF8))
@@ -164,6 +260,10 @@ class ControlMainWindow(QtGui.QMainWindow):
         self.ui.rbAuto.clicked.connect(self.handleConnectionTypeAuto)
         self.ui.rbSerial.clicked.connect(self.handleConnectionTypeSerial)
         self.ui.btnConnect.clicked.connect(self.handleConnectButton)
+
+        # Setup validation UI
+        self.ui.btnValidationRunTests.clicked.connect(self.handleRunValidation)
+        self.ui.btnValidationSetDefaults.clicked.connect(self.handleSetDefaults)
 
         # MAVLink
         self.mavport = None
@@ -205,6 +305,9 @@ class ControlMainWindow(QtGui.QMainWindow):
             self.ui.rbSerial.setEnabled(True)
             self.ui.btnConnect.setText("Connect")
             self.ui.tabWidget.setEnabled(False)
+
+    def resetUI(self):
+        self.clearValidationResults()
 
     def setConnectionStatusBanner(self, state):
         if state == 'disconnected':
@@ -252,6 +355,108 @@ class ControlMainWindow(QtGui.QMainWindow):
             self.closeConnection()
             self.setConnectionState(False)
 
+    @Slot()
+    def handleRunValidation(self):
+        if self.connected:
+            self.runAsyncValidation()
+
+    @Slot()
+    def handleSetDefaults(self):
+        if self.connected:
+            self.runAsyncSetDefaults()
+
+    def validateVersion(self):
+        return setup_validate.validate_version(self.link)
+
+    def validateCommutation(self):
+        return setup_validate.validate_comutation(self.link)
+    
+    def validateJoints(self):
+        return setup_validate.validate_joints(self.link)
+    
+    def validateGyros(self):
+        return setup_validate.validate_gyros(self.link)
+    
+    def validateAccelerometers(self):
+        return setup_validate.validate_accelerometers(self.link)
+    
+    def validateGains(self):
+        return setup_validate.validate_gains(self.link)
+
+    def setDefaults(self):
+        return setup_validate.restore_defaults(self.link)
+
+    def updateValidation(self, result, uiLabel, failureMessage=""):
+        if result == setup_validate.Results.Pass:
+            uiLabel.setText("PASS")
+            uiLabel.setStyleSheet("color: rgb(0, 0, 0);\n"
+                "background-color: rgb(0, 255, 0);")
+        elif result == setup_validate.Results.Fail:
+            uiLabel.setText("FAIL (%s)" % failureMessage)
+            uiLabel.setStyleSheet("color: rgb(255, 255, 255);\n"
+                "background-color: rgb(255, 0, 0);")
+        elif result == setup_validate.Results.Error:
+            uiLabel.setText("ERROR")
+            uiLabel.setStyleSheet("color: rgb(255, 255, 255);\n"
+                "background-color: rgb(255, 0, 0);")
+        else:
+            uiLabel.setText("")
+            uiLabel.setStyleSheet("color: rgb(0, 0, 0);\n"
+                "background-color: rgb(255, 255, 255);")
+
+    def clearValidationResults(self):
+        self.updateValidation(None, self.ui.lblValidationVersion)
+        self.updateValidation(None, self.ui.lblValidationCommutation)
+        self.updateValidation(None, self.ui.lblValidationJoints)
+        self.updateValidation(None, self.ui.lblValidationGyros)
+        self.updateValidation(None, self.ui.lblValidationAccelerometer)
+        self.updateValidation(None, self.ui.lblValidationGains)
+
+    @coroutine
+    def runAsyncValidation(self):
+        self.ui.btnValidationRunTests.setEnabled(False)
+        self.ui.btnValidationSetDefaults.setEnabled(False)
+        self.clearValidationResults()
+
+        valid = yield AsyncTask(self.validateVersion)
+        self.updateValidation(valid, self.ui.lblValidationVersion,
+            "Update to latest gimbal software")
+
+        valid = yield AsyncTask(self.validateCommutation)
+        self.updateValidation(valid, self.ui.lblValidationCommutation,
+            "Run comutation calibration")
+
+        valid = yield AsyncTask(self.validateJoints)
+        self.updateValidation(valid, self.ui.lblValidationJoints,
+            "Run joint calibration")
+
+        valid = yield AsyncTask(self.validateGyros)
+        self.updateValidation(valid, self.ui.lblValidationGyros,
+            "Run gyro calibration")
+
+        valid = yield AsyncTask(self.validateAccelerometers)
+        self.updateValidation(valid, self.ui.lblValidationAccelerometer,
+            "Run accelerometer calibration")
+
+        valid = yield AsyncTask(self.validateGains)
+        self.updateValidation(valid, self.ui.lblValidationGains,
+            "Reset parameters to default values")
+
+        self.ui.btnValidationRunTests.setEnabled(True)
+        self.ui.btnValidationSetDefaults.setEnabled(True)
+
+    @coroutine
+    def runAsyncSetDefaults(self):
+        self.ui.btnValidationRunTests.setEnabled(False)
+        self.ui.btnValidationSetDefaults.setEnabled(False)
+        self.clearValidationResults()
+
+        result = yield AsyncTask(self.setDefaults)
+        print("defaults", result)
+
+        self.ui.btnValidationRunTests.setEnabled(True)
+        self.ui.btnValidationSetDefaults.setEnabled(True)
+
     def autoConnectionWorker(self):
         return setup_mavlink.open_comm()
 
@@ -291,6 +496,7 @@ class ControlMainWindow(QtGui.QMainWindow):
     def closeConnection(self):
         self.link.file.close()
         self.setStatusInfo(None)
+        self.resetUI()
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
