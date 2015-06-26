3a63c064ec7f182ea55833c27ebfc78576cd8802
Angus Peart <angusp@gmail.com>
2015-06-26 17:15:34 +0800

    tools: force redraw of mouse cursor in gui

--- Tools/src/gui_utils.py
+++ Tools/src/gui_utils.py
@@ -6,6 +6,9 @@ def getComboboxSelection(combobox):
 def waitCursor(function):
     def wrapper(*args, **kwargs):
         QtGui.QApplication.setOverrideCursor(QtGui.QCursor(QtCore.Qt.WaitCursor))
+        # Update the cursor position to the restoreOverrideCursor() method update on screen
+        x, y = QtGui.QCursor.pos().x(), QtGui.QCursor.pos().y()
+        QtGui.QCursor.setPos(x, y)
         result = function(*args, **kwargs)
         QtGui.QApplication.restoreOverrideCursor()
         # Update the cursor position to the restoreOverrideCursor() method update on screen
