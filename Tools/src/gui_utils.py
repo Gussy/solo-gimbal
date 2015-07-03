5e6270b6821e2a6bc393f5521aed57974bcf07d4
Angus Peart <angusp@gmail.com>
2015-07-03 11:31:10 +0800

    tools: remove gui monkey-patching for linux compat

--- Tools/src/gui_utils.py
+++ Tools/src/gui_utils.py
@@ -5,14 +5,14 @@ def getComboboxSelection(combobox):
 
 def waitCursor(function):
     def wrapper(*args, **kwargs):
-        QtGui.QApplication.setOverrideCursor(QtGui.QCursor(QtCore.Qt.WaitCursor))
-        # Update the cursor position to the restoreOverrideCursor() method update on screen
-        x, y = QtGui.QCursor.pos().x(), QtGui.QCursor.pos().y()
-        QtGui.QCursor.setPos(x, y)
+        # QtGui.QApplication.setOverrideCursor(QtGui.QCursor(QtCore.Qt.WaitCursor))
+        # # Update the cursor position to the restoreOverrideCursor() method update on screen
+        # x, y = QtGui.QCursor.pos().x(), QtGui.QCursor.pos().y()
+        # QtGui.QCursor.setPos(x, y)
         result = function(*args, **kwargs)
-        QtGui.QApplication.restoreOverrideCursor()
-        # Update the cursor position to the restoreOverrideCursor() method update on screen
-        x, y = QtGui.QCursor.pos().x(), QtGui.QCursor.pos().y()
-        QtGui.QCursor.setPos(x, y)
+        # QtGui.QApplication.restoreOverrideCursor()
+        # # Update the cursor position to the restoreOverrideCursor() method update on screen
+        # x, y = QtGui.QCursor.pos().x(), QtGui.QCursor.pos().y()
+        # QtGui.QCursor.setPos(x, y)
         return result
     return wrapper
