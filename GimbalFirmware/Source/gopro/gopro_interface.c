6c9625fcfe60a32f92c18be1d229d4e88ea8c077
Angus Peart <angusp@gmail.com>
2015-07-06 14:54:32 +0800

    gopro: fixup logic in gopro detection

--- GimbalFirmware/Source/gopro/gopro_interface.c
+++ GimbalFirmware/Source/gopro/gopro_interface.c
@@ -122,9 +122,10 @@ GPHeartbeatStatus gp_get_heartbeat_status()
 			&& last_set_request.cmd_id == GOPRO_COMMAND_SHUTTER
 			&& last_set_request.value == 1) {
 			heartbeat_status = GP_HEARTBEAT_RECORDING;
-	} else */if (gp_get_power_status() == GP_POWER_ON && gp_ready_for_cmd() && gccb_version_queried == 1) {
+	} else */
+	if (gp_get_power_status() == GP_POWER_ON && gp_ready_for_cmd() && gccb_version_queried == 1) {
 		heartbeat_status = GP_HEARTBEAT_CONNECTED;
-	} else if (gp_get_power_status() != GP_POWER_ON && !i2c_get_bb()) {
+	} else if (gp_get_power_status() != GP_POWER_ON && !i2c_get_bb() && GP_VON) {
 		// If the power isn't 'on' but the I2C lines are still pulled high, it's likely an incompatible Hero 4 firmware
 		heartbeat_status = GP_HEARTBEAT_INCOMPATIBLE;
 	} else if (gp_get_power_status() == GP_POWER_ON && gccb_version_queried == 0) {
