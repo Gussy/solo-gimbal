bc1fba5bf4891106381457131de30f3891d1253f
Aaron Bamberger <abamberger@aesaustin.com>
2015-07-15 17:47:51 -0500

    Added checksum validation of loaded flash params struct

--- shared/headers/flash/flash.h
+++ shared/headers/flash/flash.h
@@ -16,4 +16,9 @@ int write_flash(void);
  */
 int init_flash(void);
 
+/**
+ * Compute the 16-bit checksum of the flash parameters struct, return the checksum
+ */
+Uint16 compute_flash_params_checksum();
+
 #endif /* FLASH_H_ */
