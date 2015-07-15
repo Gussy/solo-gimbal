3e38cb60b09a9eca5acbe6f90ae676f01a6e6926
Igor Napolskikh <igor.nap@gmail.com>
2015-07-14 22:08:03 -0700

    merge: complete merge

--- GimbalFirmware/Headers/gopro/gopro_interface.h
+++ GimbalFirmware/Headers/gopro/gopro_interface.h
@@ -58,37 +58,6 @@ typedef enum{
     GP_MODEL_UNKNOWN
 } GPModel;
 
-#if 0
-// included in gopro_hero_common.h
-typedef enum {
-    GP_CMD_UNKNOWN,
-    GP_CMD_SUCCESSFUL,
-    GP_CMD_SEND_CMD_START_TIMEOUT,
-    GP_CMD_SEND_CMD_COMPLETE_TIMEOUT,
-    GP_CMD_GET_RESPONSE_START_TIMEOUT,
-    GP_CMD_GET_RESPONSE_COMPLETE_TIMEOUT,
-    GP_CMD_GET_CMD_COMPLETE_TIMEOUT,
-    GP_CMD_SEND_RESPONSE_START_TIMEOUT,
-    GP_CMD_SEND_RESPONSE_COMPLETE_TIMEOUT,
-    GP_CMD_PREEMPTED,
-    GP_CMD_RECEIVED_DATA_OVERFLOW,
-    GP_CMD_RECEIVED_DATA_UNDERFLOW
-} GPCmdResult;
-
-typedef enum {
-    GP_CMD_STATUS_SUCCESS = 0,
-    GP_CMD_STATUS_FAILURE = 1,
-    GP_CMD_STATUS_UNKNOWN = 2
-} GPCmdStatus;
-
-
-
-typedef enum {
-    GP_REQUEST_NONE,
-    GP_REQUEST_GET,
-    GP_REQUEST_SET
-} GPRequestType;
-
 // XXX: what should this actually be?
 #define GP_MAX_RESP_LEN     32
 
@@ -99,35 +68,6 @@ typedef struct {
     uint16_t payload[GP_MAX_RESP_LEN];
     uint16_t len;
 } gp_response_t;
-
-typedef struct {
-    char cmd[2];
-    Uint8 cmd_parm;
-} GPCmd;
-
-typedef struct {
-    char cmd[2];
-    GPCmdStatus status; // as reported by the camera
-    Uint8 value;
-    GPCmdResult result; // of the i2c transaction itself
-} GPCmdResponse;
-
-typedef struct {
-    Uint8 cmd_id;
-    Uint8 value;
-} GPSetRequest;
-
-typedef struct {
-    Uint8 cmd_id;
-    Uint8 value;
-} GPGetResponse;
-
-typedef struct {
-    Uint8 cmd_id;
-    Uint8 result;
-} GPSetResponse;
-#endif
-
 
 void init_gp_interface();
 void gp_interface_state_machine();
