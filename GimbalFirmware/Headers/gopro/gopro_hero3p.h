ef78adddb4dc8e523baae34e160c29a8797ad2f5
Igor Napolskikh <igor.nap@gmail.com>
2015-07-15 05:03:11 -0700

    gopro_interface: explicitly define hero3+ msg format

--- GimbalFirmware/Headers/gopro/gopro_hero3p.h
+++ GimbalFirmware/Headers/gopro/gopro_hero3p.h
@@ -9,6 +9,9 @@
 // Include for GOPRO_COMMAND enum
 #include "mavlink_interface/mavlink_gimbal_interface.h"
 
+#define GP_H3P_MAX_PACKET        256
+#define GP_H3P_MAX_PAYLOAD       (GP_H3P_MAX_PACKET - 5)
+
 // Hero 3 related state
 typedef struct {
     // TODO
@@ -18,13 +21,27 @@ typedef struct {
 
 // Request (CMD) General Format
 typedef struct {
-    // TODO
+    uint16_t len;     // size of this packet (not including 'len'), also contains requester id (Camera or Bac-Pac) in most significant bit
+    uint16_t cmd1;    // first letter of 2-letter cmd
+    uint16_t cmd2;    // second letter of 2-letter cmd
+    uint16_t payload[GP_H3P_MAX_PAYLOAD - 3]; // known as 'params' in documentation // TODO: keep this consistent with hero4 naming or call params?
 } gp_h3p_cmd_t;
 
 // Response (RSP) General Format
 typedef struct {
-    // TODO
+    uint16_t len;       // size of this packet (not including 'len')
+    uint16_t status;    // successful/unsuccessful
+    uint16_t payload[GP_H3P_MAX_PAYLOAD - 2];
 } gp_h3p_rsp_t;
+
+typedef union {
+
+    gp_h3p_cmd_t cmd;
+    gp_h3p_rsp_t rsp;
+
+    uint16_t bytes[GP_H3P_MAX_PACKET];
+
+} gp_h3p_pkt_t;
 
 bool gp_h3p_request_power_off();
 bool gp_h3p_cmd_has_param(const GPCmd* c);
