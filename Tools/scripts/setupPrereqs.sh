6bfbed0dd6a5901d2c020f95de0708fa10572c58
Angus Peart <angusp@gmail.com>
2015-07-27 18:16:02 +0800

    travis: use travis' new build infrastructure

--- Tools/scripts/setupPrereqs.sh
+++ Tools/scripts/setupPrereqs.sh
@@ -8,9 +8,6 @@ OPT="$HOME/opt"
 TI_TARBALL="ti-cgt-c2000_6.4.2.tar.gz"
 TI_TARBALL_URL="http://gimbal-ci.s3-website-us-east-1.amazonaws.com/compiler/ti-cgt-c2000_6.4.2.tar.gz"
 
-sudo apt-get update -y -qq
-sudo apt-get install gcc-multilib -y -qq
-
 mkdir -p $OPT
 cd $OPT
 wget $TI_TARBALL_URL
